d8336a9cc7161074284793e40b79ee38bc22c4df
rexstatic <paw94@live.dk>
2020-07-21 03:13:26 +0200

    Fix of main test

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 when beginning with git ended up deleting 150 lines of code.... 
 And it was removed from cloud and other devices b4 i could save it
+
+
+this is a test
+
+except expression as identifier:
+    pass
+else:
+    pass
+
+of git
